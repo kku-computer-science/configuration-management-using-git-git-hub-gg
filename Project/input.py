a44d8cb6f8526c5466b6c16a7e9c99fc567ba041
ญานวิทย์ <yanawit.p@kkumail.com>
2025-12-09 21:47:50 +0700

    Update input.py to work in Main (basically change the whole thing)
    real

--- Project/input.py
+++ Project/input.py
@@ -2,12 +2,8 @@
 ของ ญานวิทย์ พิชญกุลมงคล
 หน้าที่: input
 """
-from output import sort_numbers
-numbers = input("Enter integers separated by space: ")
-algorithm = input("Choose algorithm (quick/bubble): ").lower()
-result = sort_numbers(numbers, algorithm)
-if result is None:
-    print("Invalid algorithm.")
-else:
-    print("Original:", numbers)
-    print(f"Sorted using {algorithm} sort:", result)
+def get_user_input():
+    numbers = input("Enter integers separated by space: ")
+    algorithm = input("Choose algorithm (quick/bubble): ").lower()
+    return numbers, algorithm
+
