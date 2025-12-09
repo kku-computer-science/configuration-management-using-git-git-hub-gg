d4713d8d41050476e999dbdc83f3761e4b89580b
NATTAPAT VORAJINDA <nattapat.v@kkumail.com>
2025-12-09 21:50:13 +0700

    Add Nattapat profile and Quick sort implementtation and main

--- Project/Main.py
+++ Project/Main.py
@@ -1,1 +1,20 @@
-"placeholder"
+"""
+ของ ณัฐภัทร วรจินดา
+หน้าที่: Main
+"""
+from input import get_user_input
+from output import sort_numbers
+
+def main():
+    numbers, algorithm = get_user_input()
+    result = sort_numbers(numbers, algorithm)
+
+    if result is None:
+        print("Invalid algorithm. Choose 1, 2, b, q, bubble, or quick.")
+    else:
+        print("Original:", numbers)
+        print("Sorted result:", result)
+
+
+if __name__ == "__main__":
+    main()
